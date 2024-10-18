eb31cc201f953c0bf22e62cc77e52ff6ff578386
Juan Lischetti <lischetti.juan@gmail.com>
2024-10-18 20:01:59 -0300

    implemented documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Star Wars Film Manager API')
+    .setDescription(
+      'Documentación de la API para gestionar las películas de Star Wars',
+    )
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('users')
+    .addTag('films')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -1,39 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsEnum,
   IsNotEmpty,
   IsString,
   IsStrongPassword,
+  Matches,
 } from 'class-validator';
 import { Match } from 'src/utils/decorators/match.decorator';
 import { USER_ROLES } from 'src/utils/enums/roles';
 
 export class SignUpDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'user@example.com' })
   @IsNotEmpty()
   @IsEmail()
   @Match('email')
   confirmEmail: string;
 
+  @ApiProperty({ example: 'John' })
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[A-Za-z\s]+$/, {
+    message: 'Name can only contain letters and spaces for compound names',
+  })
   firstName: string;
 
+  @ApiProperty({ example: 'Doe' })
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[A-Za-z\s]+$/, {
+    message: 'Name can only contain letters and spaces for compound names',
+  })
   lastName: string;
 
+  @ApiProperty({ example: 'Abcd1234.' })
   @IsNotEmpty()
   @IsStrongPassword()
   password: string;
 
+  @ApiProperty({ example: 'Abcd1234.' })
   @IsNotEmpty()
   @Match('password')
   confirmPassword: string;
 
+  @ApiProperty({
+    enum: USER_ROLES,
+    example: USER_ROLES.USER,
+  })
   @IsNotEmpty()
   @IsEnum(USER_ROLES, { message: 'Role must be ADMIN or USER' })
   role: USER_ROLES;
